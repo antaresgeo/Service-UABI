aa1ff8d19e7d82192e0862d6c6d425693b9e21ce
Santiago Suárez <santiago.suarez@axcelsoftware.com>
2021-11-24 08:29:21 -0500

    Bugfix

--- start/routes/index.ts
+++ start/routes/index.ts
@@ -1,11 +1,6 @@
-// import "./project";
-// import "./realEstate";
-// import "./acquisition";
-// import "./insurability";
-// import "./insuranceBroker";
-// import "./insuranceCompany";
-import Route from "@ioc:Adonis/Core/Route";
-
-Route.get("", (ctx) => {
-  ctx.response.send("works");
-});
+import "./project";
+import "./realEstate";
+import "./acquisition";
+import "./insurability";
+import "./insuranceBroker";
+import "./insuranceCompany";
